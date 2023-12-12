dbb7a02af25d9f0b6500c817ecadd3e7b6a862fb
124476 <124476@bk.ru>
2023-12-12 19:43:53 +0300

    Добавьте файлы проекта.

---
+++ WorldSkills2014/Pages/Chat.xaml.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WorldSkills2014.AppWindow;
+using WorldSkills2014.Models;
+
+namespace WorldSkills2014.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для Chat.xaml
+    /// </summary>
+    public partial class Chat : Page
+    {
+        ChatroomMembers chatroomMember;
+        public Chat(ChatroomMembers chatroomMembers)
+        {
+            InitializeComponent();
+            chatroomMember = chatroomMembers;
+            Topic.Text = "Topic: " + chatroomMembers.Chatroom.Topic;
+        }
+
+        private void Send_Click(object sender, RoutedEventArgs e)
+        {
+            if (Text.Text != null)
+            {
+                ChatMessage mess = new ChatMessage();
+                mess.Date = DateTime.Now;
+                mess.Messange = Text.Text;
+                mess.Charroom_Id = chatroomMember.Chatroom_Id;
+                mess.Sender_Id = chatroomMember.Employee_Id;
+                App.DB.ChatMessage.Add(mess);
+                App.DB.SaveChanges();
+                NavigationService.GoBack();
+                NavigationService.Navigate(new Chat(chatroomMember));
+            }
+        }
+
+        private void AddUSer_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Poisk(null, chatroomMember));
+        }
+
+        private void Change_Click(object sender, RoutedEventArgs e)
+        {
+            var window = new RedactTopic(chatroomMember);
+            var result = window.ShowDialog();
+            if (result == true)
+            {
+                NavigationService.GoBack();
+                NavigationService.Navigate(new Chat(chatroomMember));
+            }
+        }
+
+        private void Leave_Click(object sender, RoutedEventArgs e)
+        {
+            if (App.DB.ChatroomMembers.Where(x => x.Chatroom_Id == chatroomMember.Chatroom_Id).Count() <= 2)
+            {
+                ChatroomMembers chatmember2 = App.DB.ChatroomMembers.FirstOrDefault(x =>
+                x.Chatroom_Id == chatroomMember.Chatroom_Id && x.Id != chatroomMember.Id);
+                App.DB.ChatroomMembers.Remove(chatmember2);
+            }
+            App.DB.ChatroomMembers.Remove(chatroomMember);
+            App.DB.SaveChanges();
+            NavigationService.Navigate(new Main(App.user));
+        }
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            DGUser.ItemsSource = App.DB.Employee.Where(x => App.DB.ChatroomMembers.Where(s => s.Employee_Id == x.Id &&
+            s.Chatroom_Id == chatroomMember.Chatroom_Id).Count() != 0).ToList();
+            DGChat.ItemsSource = App.DB.ChatMessage.Where(x => x.Charroom_Id == chatroomMember.Chatroom_Id).ToList();
+        }
+
+        private void Back_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Main(App.user));
+        }
+    }
+}
---
+++ WorldSkills2014/Pages/Poisk.xaml.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WorldSkills2014.Models;
+
+namespace WorldSkills2014.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для Poisk.xaml
+    /// </summary>
+    public partial class Poisk : Page
+    {
+        List<Department> departs;
+        Employee user;
+        ChatroomMembers chatroomMember;
+        public Poisk(Employee comuser, ChatroomMembers chatroomMembers)
+        {
+            InitializeComponent();
+            user = comuser;
+            chatroomMember = chatroomMembers;
+            if (user != null)
+            {
+                DGPerson.ItemsSource = App.DB.Employee.Where(x => x.Id != user.Id).ToList();
+            }
+            else
+            {
+                DGPerson.ItemsSource = App.DB.Employee.Where(x => App.DB.ChatroomMembers.Where(s => s.Chatroom_Id
+                == chatroomMember.Chatroom_Id && s.Employee_Id == x.Id).Count() == 0).ToList();
+            }
+            
+            DGDepart.ItemsSource = App.DB.Department.ToList();
+            
+            departs = new List<Department>();
+            foreach (Department depart in App.DB.Department)
+            {
+                departs.Add(depart);
+            }
+        }
+
+        private void StrokaPoist_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+            Refresh();
+        }
+
+        private void Refresh()
+        {
+            if (user != null)
+            {
+                var filtered = App.DB.Employee.Where(x => x.Id != user.Id).ToList();
+            
+                var select = StrokaPoist.Text.ToLower();
+                filtered = filtered.Where(s => departs.FirstOrDefault(x => x == s.Department) != null).ToList();
+
+                if (string.IsNullOrWhiteSpace(select) == false)
+                {
+                    filtered = filtered.Where(s => s.FullName.ToLower().Contains(select)).ToList();
+                }
+                DGPerson.ItemsSource = filtered;
+            }
+            else
+            {
+                var filtered = App.DB.Employee.Where(x => App.DB.ChatroomMembers.Where(s => s.Chatroom_Id 
+                == chatroomMember.Chatroom_Id && s.Employee_Id == x.Id).Count() == 0).ToList();
+
+                var select = StrokaPoist.Text.ToLower();
+                filtered = filtered.Where(s => departs.FirstOrDefault(x => x == s.Department) != null).ToList();
+
+                if (string.IsNullOrWhiteSpace(select) == false)
+                {
+                    filtered = filtered.Where(s => s.FullName.ToLower().Contains(select)).ToList();
+                }
+                DGPerson.ItemsSource = filtered;
+            }
+        }
+
+        private void GetUser_Click(object sender, RoutedEventArgs e)
+        {
+            if (DGPerson.SelectedItem is Employee depart)
+            {
+                if (user != null)
+                {
+                    Chatroom newroom = new Chatroom();
+                    newroom.Topic = user.Name + " - " + depart.Name;
+
+                    ChatroomMembers newChat = new ChatroomMembers();
+                    ChatroomMembers newChat2 = new ChatroomMembers();
+                    newChat.Employee = depart;
+                    newChat2.Employee = user;
+                    newChat.Chatroom = newroom;
+                    newChat2.Chatroom = newroom;
+                    App.DB.Chatroom.Add(newroom);
+                    App.DB.ChatroomMembers.Add(newChat);
+                    App.DB.ChatroomMembers.Add(newChat2);
+                    App.DB.SaveChanges();
+                    NavigationService.Navigate(new Chat(newChat2));
+                }
+                else
+                {
+                    ChatroomMembers newChat = new ChatroomMembers();
+                    newChat.Employee = depart;
+                    newChat.Chatroom = chatroomMember.Chatroom;
+                    App.DB.ChatroomMembers.Add(newChat);
+                    App.DB.SaveChanges();
+                    NavigationService.Navigate(new Chat(chatroomMember));
+                }
+            }
+        }
+
+        private void ItUser_Unchecked(object sender, RoutedEventArgs e)
+        {
+            if (DGDepart.SelectedItem is Department depart)
+            {
+                departs.Remove(depart);
+                Refresh();
+            }
+        }
+
+        private void ItUser_Checked(object sender, RoutedEventArgs e)
+        {
+            if (DGDepart.SelectedItem is Department depart)
+            {
+                departs.Add(depart);
+                Refresh();
+            }
+        }
+    }
+}
